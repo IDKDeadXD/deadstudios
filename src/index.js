36d84925274dc01dcdc385526d01f74a33e1419b
IDKDead <johnnyhaire97@gmail.com>
2025-01-03 20:42:51 -0600

    1.0.0-BETA

--- src/index.js
+++ src/index.js
@@ -1,11 +1,15 @@
+// Option 2 - Alternative approach with null check
 import React from 'react';
-import ReactDOM from 'react-dom/client';
+import { createRoot } from 'react-dom/client';
 import './index.css';
 import App from './App';
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
-);
+const container = document.getElementById('root');
+if (container) {
+  const root = createRoot(container);
+  root.render(
+    <React.StrictMode>
+      <App />
+    </React.StrictMode>
+  );
+}
