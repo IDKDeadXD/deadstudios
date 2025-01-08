74fc365ec5cbecbaef8757701b90d44b9ddf7e7c
IDKDead <johnnyhaire97@gmail.com>
2025-01-08 17:31:13 -0600

    fix

--- vite.config.js
+++ vite.config.js
@@ -1,9 +1,10 @@
 import { defineConfig } from 'vite';
 
 export default defineConfig({
-  root: 'src', // Set 'src' as the root
+  root: './src',
+  base: './', // Ensures relative paths for assets
   build: {
-    outDir: '../public', // Output the build to the 'public' folder
-    emptyOutDir: true, // Clean the output directory before building
+    outDir: 'dist', // Or 'build' if you changed it for Vercel
+    emptyOutDir: true,
   },
 });
