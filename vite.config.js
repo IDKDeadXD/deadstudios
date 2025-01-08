d2f02ad0b340e2e8d31ea8fa658f42d57cb8e6a7
IDKDead <johnnyhaire97@gmail.com>
2025-01-08 17:42:08 -0600

    1.0.3

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,8 @@
 import { defineConfig } from 'vite';
 
 export default defineConfig({
-  base: './', // Ensures relative paths for assets
+  root: './src/',
+  base: './src/', // Ensures relative paths for assets
   build: {
     outDir: 'dist', // Or 'build' if you changed it for Vercel
     emptyOutDir: true,
